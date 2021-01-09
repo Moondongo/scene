e1b883f23aa3ffb6f75e75337e9a7ed33cfa3170
Moondongo <francoalbornoz@alu.frt.utn.edu.ar>
2021-01-09 19:52:36 -0300

    agregado horno

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 import * as THREE from './modulos/three.module.js';
 import {OrbitControls} from './modulos/OrbitControls.js';
 import {GLTFLoader} from './modulos/GLTFLoader.js';
-
+import {RGBELoader} from './modulos/RGBELoader.js';
 let scene, camera, renderer, controls;
-
+let pmremGenerator;
+let loadingManager;
 init();
 function init(){
     createScene();
     createCamera();
     createRenderer();
+    loadManager();
     createControls();
-    cargarModelo('3dmodel/bankito/Banco.gltf');
+    //cargarModelo('3dmodel/smile/WaterBottle.glb');
+    cargarModelo('3dmodel/horno/horno.glb');
     lighting();
     render();
+    window.addEventListener('resize', onWindowResize, false);
 }
 
 function createScene(){
@@ -22,18 +26,21 @@ function createScene(){
 }
 function createCamera(){
     camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 100);
-    camera.position.z = 6;
-    camera.position.y = 5;
+    camera.position.z = 2;
+    camera.position.y = 0.9;
+    camera.position.x = 1;
 }
 function createRenderer(){
     renderer = new THREE.WebGLRenderer({antialias:true});
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
+    pmremGenerator = new THREE.PMREMGenerator( renderer );
+	pmremGenerator.compileEquirectangularShader();
 }
 function createControls(){
     controls = new OrbitControls(camera, renderer.domElement);
     controls.minDistance = 2;
-    controls.maxDistance = 10;
+    controls.maxDistance = 4;
     controls.enableDamping = true;
     controls.dampingFactor = 0.22;
     controls.addEventListener('change', render);
@@ -43,7 +50,7 @@ function render(){
 }
 
 function cargarModelo(patch){
-    const loader = new GLTFLoader();
+    const loader = new GLTFLoader(loadingManager);
     loader.load(patch, gltf =>{
         scene.add(gltf.scene);
         render();
@@ -53,10 +60,10 @@ function cargarModelo(patch){
 }
 
 function lighting(){
-    const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.1);
+    const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
     scene.add(directionalLight);
 
-    const light = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.8);
+    const light = new THREE.HemisphereLight(0xffffff, 0xffffff, 1);
     scene.add(light);
 
     const ambient = new THREE.AmbientLight(0x352847);
@@ -64,11 +71,35 @@ function lighting(){
 }
 
 function loadHDRI(){
-    const loader = new THREE.TextureLoader();
-    loader.load('hdri/forest.jpg', texture =>{
-        const rt = new THREE.WebGLCubeRenderTarget(texture.image.height);
-        rt.fromEquirectangularTexture(renderer, texture);
-        scene.background = rt;
-        render();
+    new RGBELoader()
+        .setDataType(THREE.UnsignedByteType)
+        .setPath('hdri/')
+        .load('studio.hdr', texture =>{
+            const envMap = pmremGenerator.fromEquirectangular(texture).texture;
+            scene.background = envMap;
+            scene.environment = envMap;
+            texture.dispose();
+            pmremGenerator.dispose();
+            render();
+        });
+}
+
+function onWindowResize() {
+
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+    renderer.setSize( width, height );
+    render();
+}
+
+function loadManager(){
+    loadingManager = new THREE.LoadingManager(()=>{
+        const loadingScreen = document.getElementById('loading-screen');
+        loadingScreen.classList.add('fade-out');
+        loadingScreen.addEventListener('transitionend', e =>{
+            e.target.remove();
+        });
     });
 }
